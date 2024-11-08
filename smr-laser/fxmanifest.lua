9c94bc62838aa4e4a9a859f792c5a9a8f1bddeb1
Samurai4516 <ryan.gryszko2@gmail.com>
2024-11-08 16:53:24 +0100

    Initial commit

---
+++ smr-laser/fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+
+game 'gta5'
+lua54 'yes'
+author 'Samurai'
+
+
+
+
+client_scripts {
+    'client/opensource.lua',
+    'client/functions.lua',
+	'client/client.lua',
+}
+
+server_scripts {
+   '@oxmysql/lib/MySQL.lua',
+	'server/server.lua',
+	'server/opensource.lua',
+}
+
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+escrow_ignore {
+    'config.lua',
+    'server/opensource.lua',
+    'client/opensource.lua'
+}
